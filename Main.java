e3c199439751a91ad6457804ab077fd3f37e13ba
Hager <hagertaher257@gmail.com>
2024-11-06 15:22:54 +0200

    test

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.Date;
+
+
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("hello world");
+    }
+}
